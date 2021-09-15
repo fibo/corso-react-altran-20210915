50267a44bcce8afc211e5e7cc6d2eec3920cff4f
Alfonso <a.delgaizo2@gmail.com>
2021-09-15 16:35:07 +0200

    add terms service

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,14 +3,26 @@ import {Centered} from "../components/Centered"
 
 export function Login() {
     const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [submitIsDisabled, setSubmitIsDisabled] = useState(true)
 
     const onChangeEmail = (event) => {
         setEmail(event.target.value)
     }
 
+    const onChangePassword = (event) => {
+        setPassword(event.target.value)
+    }
+
     const onSubmit = (event) => {
         event.preventDefault()
-        console.log(email)
+        if (submitIsDisabled) return 
+        console.log(email, password)
+    }
+
+    const onChangeCheckBox = (event) => {
+        setSubmitIsDisabled(!event.target.checked)
+        console.log(event.target.checked)
     }
 
     return (
@@ -19,24 +31,40 @@ export function Login() {
                 <div className="field">
                     <label className="label">email</label>
                     <div className="control">
-                        <input className="input" type="email"
+                        <input 
+                            className="input" 
+                            type="email"
                             onChange={onChangeEmail}
-                            onFocus={() => {
-                                console.log("Focus")
-                            }}/>
+                         />
                     </div>
                 </div>
                 <div className="field">
                     <label className="label">password</label>
                     <div className="control">
-                        <input className="input" type="password"/>
+                        <input 
+                        className="input" 
+                        type="password"
+                        onChange={onChangePassword}
+                        />
                     </div>
                 </div>
+
+                <div className="field">
+                    <div className="control">
+                        <label className="checkbox">
+                        <input type="checkbox" onChange={onChangeCheckBox}/>
+                        &nbsp;I agree to the <a href="#">terms and conditions</a>
+                        </label>
+                    </div>
+                </div>
+
+
                 <div className="field is-grouped">
                     <div className="control">
-                        <input type="submit" className="button is-link" value="Enter"/>
+                        <input type="submit" className="button is-link" disabled={submitIsDisabled} value="Enter"/>
                     </div>
                 </div>
+                
             </form>
         </Centered>
     )
