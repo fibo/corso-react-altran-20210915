6fca233d581d3c3b3c5b091c6b529651d91a3a61
Bombaclath97 <e.bombardelli97@gmail.com>
2021-09-15 15:35:53 +0200

    Add form and input handle

---
+++ src/components/Centered.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Centered({ children }) {
+    return (<div className="centered">{ children }</div>)
+}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,45 @@
-import React from "react";
-import { Button } from "../components/Button";
-export function Login(){
-    const buttonLabel = 'Send';
-    const onClick= () => {
-        console.log("click")
+import React, {useState} from "react"
+import {Centered} from "../components/Centered"
+
+export function Login() {
+    const [email, setEmail] = useState('')
+
+    const onChangeEmail = (event) => {
+        setEmail(event.target.value)
     }
-    return (<div>
-        <Button label={buttonLabel} onClick={onClick} isPrimary/>
-        <Button label="Cancel" />
-        <Button isLoading />
-    </div>
-        
-    );
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        console.log(email)
+    }
+
+    return (
+        <Centered>
+            <form onSubmit={onSubmit}>
+                <div className="field">
+                    <label className="label">email</label>
+                    <div className="control">
+                        <input className="input" type="email"
+                            onChange={onChangeEmail}
+                            onFocus={() => {
+                                console.log("Focus")
+                            }}/>
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">password</label>
+                    <div className="control">
+                        <input className="input" type="password"/>
+                    </div>
+                </div>
+                <div className="field is-grouped">
+                    <div className="control">
+                        <input type="submit" className="button is-link" value="Enter"/>
+                    </div>
+                </div>
+            </form>
+        </Centered>
+    )
 }
+
+// onChange, onFocus, onBlur
